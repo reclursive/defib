7d389505feef637768eb5ab064d80fa4828f41c0
reclursive <imafraidofferrets@gmail.com>
2024-01-12 13:04:16 -0500

    read me expanded

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # defib
+### a multi-user headline analyzer
+### Absolutely no AI incorporated whatsoever, data generated is user based only
+### user can flag headlines or publish analysis/resources which can be liked
+### headlines most flagged appear first in infinite scroll
+### top publications are hyper linked with word or headline
+### ________Ice Box
+### sort options for infinite scroll 
+### current news and simultaneous news segments below article/headline for distraction check
+### friends - vanishing user web socket messenger
--- defib_client/src/pages/global/articles.jsx
+++ defib_client/src/pages/global/articles.jsx
@@ -11,3 +11,26 @@ const Articles= (props) => {
 }
 
 export default Articles
+
+
+// function getNumberOfIslands(binaryMatrix) {
+//     function parimCheck(matrix, x, y, rows, cols){
+      
+//     }
+    
+//       if(!binaryMatrix.length){
+//         return 0
+//       }
+//       const columns = binaryMatrix.length
+//       let total = 0
+//       for (i=0; i < binaryMatrix.length; i++){
+//         for(j=0; i<columns; i++){
+//           if (binaryMatrix[i][j] == 1){
+//             parimCheck(binaryMatrix, i, j, binaryMatrix.length, columns){
+//               total+= 1
+//             }
+//           }
+//           }
+//       }
+//       return total
+//   }
