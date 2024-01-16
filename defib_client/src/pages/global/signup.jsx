b4bb19725b8ed711493d7d8f841dc3b657b5a11f
reclursive <imafraidofferrets@gmail.com>
2024-01-16 00:52:02 -0500

    sign up form basic, bootstrap for now

--- defib_client/src/pages/global/signup.jsx
+++ defib_client/src/pages/global/signup.jsx
@@ -1,9 +1,34 @@
 const SignUp = () => {
     return (
         <div>
-            <h1>
-                sign up
-            </h1>
+            <h1>SIGN UP to DeFib</h1>
+            <form action="submit">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Username</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username"/>
+                <small id="emailHelp" class="form-text text-muted">Make it unique!</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                <small id="emailHelp" class="form-text text-muted">8+ characters, at lease one symbol and number</small>
+
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Re-enter password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password again"/>
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                <label class="form-check-label" for="exampleCheck1">Newsletter?</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
     )
 }
